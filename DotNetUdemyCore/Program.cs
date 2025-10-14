d6721476423f8b64de13bd90459275550940b30c
SamWaku <samuelwaku1st@gmail.com>
2025-10-14 09:28:22 +0200

    chore: swaggergen

---
+++ DotNetUdemyCore/Program.cs
@@ -0,0 +1,18 @@
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddOpenApi();
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddCors();
+
+var app = builder.Build();
+
+app.UseHttpsRedirection();
+app.UseSwagger();
+app.UseSwaggerUI();
+app.UseCors("Default");
+app.UseHttpsRedirection();
+app.MapControllers();
+
+app.Run();
