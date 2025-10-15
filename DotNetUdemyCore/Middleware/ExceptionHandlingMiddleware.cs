e42cb04a011d6f846a018ad73519ba0c246f2dc8
SamWaku <samuelwaku1st@gmail.com>
2025-10-15 11:06:00 +0200

    chore: invoke

---
+++ DotNetUdemyCore/Middleware/ExceptionHandlingMiddleware.cs
@@ -0,0 +1,9 @@
+﻿namespace DotNetUdemyCore.Middleware;
+
+public class ExceptionHandlingMiddleware(RequestDelegate next)
+{
+    public Task InvokeAsync(HttpContext httpContext)
+    {
+        return next(httpContext);
+    }
+}
